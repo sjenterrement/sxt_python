7647dc342b976bbf6fd198f7f8cd7d2a96c6348f
sjenterrement <sjenterrement@gmail.com>
2019-04-14 21:35:34 +0800

    Create sxt_34.py
    hh

---
+++ sxt_34.py
@@ -0,0 +1,13 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> BTS=["jnj"]
+>>> BTS.append("jsz")
+>>> BTS
+['jnj', 'jsz']
+>>> BTS+=["myq"]
+>>> BTS
+['jnj', 'jsz', 'myq']
+>>> BTS.extend(["zhx"])
+>>> BTS
+['jnj', 'jsz', 'myq', 'zhx']
+>>> 
